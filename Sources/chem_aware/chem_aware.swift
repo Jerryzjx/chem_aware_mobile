27c81f5b564bb1635bf8e8d46b23cd585093849f
JerryZhang0920 <jinxuan0920@gmail.com>
2021-11-26 20:39:15 -0500

    Rebuild for github actions

---
+++ Sources/chem_aware/chem_aware.swift
@@ -0,0 +1,6 @@
+public struct chem_aware {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/chem_awareTests/chem_awareTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import chem_aware
+
+final class chem_awareTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(chem_aware().text, "Hello, World!")
+    }
+}
