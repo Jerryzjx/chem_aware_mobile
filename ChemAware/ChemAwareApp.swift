81177f1c917802efbb76aaf5591aaa2bff5f2ed5
JerryZhang0920 <jinxuan0920@gmail.com>
2021-11-29 22:53:43 -0500

    Debug

---
+++ ChemAware/ChemAwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemAwareApp.swift
+//  ChemAware
+//
+//  Created by leonard on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct ChemAwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChemAware/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChemAware
+//
+//  Created by leonard on 2021/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
